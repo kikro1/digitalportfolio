df3a01b3a82c245717a6b64cee4aafc6f7a1d0b7
kikro1 <bogdash.voitencov.2008@gmail.com>
2024-09-24 14:51:43 +0100

    header

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ const menuBack = document.querySelector('.menu')
 burgerBtn.addEventListener('click', () => {
     menuBack.classList.toggle('open-burger-mn'); // Переключаем класс для отображения меню
 });
+
+const back = document.querySelector('.img-nav')
+burgerBtn.addEventListener('click', () => {
+    back.classList.toggle('open-burger-mn-back')
+});
